608eb42fa2c7b616147dd8a925b70422431aae7e
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2010-01-01 19:25:26 -0500

    better example settings

--- robobach/example.settings.py
+++ robobach/example.settings.py
@@ -1,5 +1,8 @@
 # Django settings for robobach project.
 # MAKE YOUR OWN COPY OF THIS named settings.py
+import os
+this_path = os.path.dirname( os.path.realpath( __file__ ) )
+BASE_PATH = os.path.join( this_path, ".." )
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,7 +13,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = ''             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
@@ -22,7 +25,7 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Vancouver'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -36,7 +39,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join( BASE_PATH, "media/" )
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -46,7 +49,7 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin_media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'e(292y*@mu4zm=szrl&%u%qfa+(v@9ooy*tkrmm^9gj3fowk&#'
@@ -67,14 +70,15 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'robobach.urls'
 
 TEMPLATE_DIRS = (
+    os.path.join( BASE_PATH, "templates" )
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.sites',
 )
