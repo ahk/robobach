8351d534b350c9be205fda99d07431f40d54ae87
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2010-03-29 01:02:42 -0700

    functional automated supercollider NRT build system

---
+++ robobach/pyCollider/super_collider/build_composition.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+from time import sleep
+from subprocess import Popen, PIPE
+from datetime import datetime
+import re
+
+now = datetime.now().strftime("%y-%m-%d_%H-%M-%S")
+client_proc = Popen(['./sclang', 'composition.scd', '&'], stdout=PIPE)
+
+client_out = ''
+terminating_pat = re.compile('composition successfully compiled')
+while not re.search(terminating_pat, client_out):
+    client_out += client_proc.stdout.readline()
+client_proc.kill()
+
+outcmd = './scsynth -N composition.osc _ ' + now + '.composition.wav 44100 WAVE int16'
+server_proc = Popen(outcmd, shell=True)
+
+server_proc.wait()
--- robobach/pyCollider/super_collider/build_composition.sh
+++
@@ -1,5 +0,0 @@
-# This isn't a real shell/python script, but it would like to be! Can you help me become automatable?
-
-./sclang composition.scd &
-sleep 3
-./scsynth -N composition.osc _ composition.wav 44100 WAVE int16
